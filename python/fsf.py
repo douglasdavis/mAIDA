873137ae0136ddb945a36eb54a3d7e3aa927d1d6
Douglas Davis <drd25@phy.duke.edu>
2014-09-11 02:24:48 -0400

    adding a little style to plotting

--- python/fsf.py
+++
@@ -1,8 +0,0 @@
-import ROOT
-ROOT.gSystem.Load('lib/libmAIDA')
-from ROOT import mAIDA
-
-fsf = mAIDA.FinalStateFiller('out.root','fs');
-fsf.AddFile('../IMLA/data/ttbarW/*.root');
-fsf.Make_osdilepton()
-fsf.Loop();
--- python/plots.py
+++ python/plots.py
@@ -2,11 +2,14 @@
 
 import ROOT
 import argparse
+import pystyle
+
+pystyle.gstyle()
 
 parser = argparse.ArgumentParser(description='Plotting from MVA output')
-parser.add_argument('-i','--in-file', help='input MVA ROOT file',required=True)
+parser.add_argument('-i','--in-file', help='input MVA ROOT file',        required=True)
 parser.add_argument('-v','--var-file',help='variables list file to plot',required=True)
-parser.add_argument('-t','--type',    help='the type of MVA used',required=True)
+parser.add_argument('-t','--type',    help='the type of MVA used',       required=True)
 args = vars(parser.parse_args())
 
 var_file  = open(args['var_file'])
@@ -18,8 +21,15 @@ infile = ROOT.TFile(args['in_file'])
 method = args['type']
 prefix = 'Method_'+method+'/'+method+'/'
 
+sfill = 3004
+bfill = 3005
+sfillc = ROOT.kGreen+1
+bfillc = ROOT.kAzure+2
+
 SignalOutput     = infile.Get(prefix+'MVA_'+method+'_S')
 BackgroundOutput = infile.Get(prefix+'MVA_'+method+'_B')
+pystyle.hstyle(SignalOutput,sfill,sfillc)
+pystyle.hstyle(BackgroundOutput,bfill,bfillc)
 
 SignalVarsOutput     = []
 BackgroundVarsOutput = []
@@ -29,14 +39,16 @@ for var in var_list:
     BackgroundVarsOutput.append(infile.Get(prefix+var+'__Background'))
 for hist in SignalVarsOutput:
     hist.SetTitle(';'+hist.GetTitle()+';Normalized')
+    pystyle.hstyle(hist,sfill,sfillc)
 for hist in BackgroundVarsOutput:
     hist.SetTitle(';'+hist.GetTitle()+';Normalized')
+    pystyle.hstyle(hist,bfill,bfillc)
     
 c_MVA = ROOT.TCanvas()
 SignalOutput.Draw('hist')
 BackgroundOutput.Draw('hist,same')
 
-cs = [ROOT.TCanvas('c'+name,'c'+name) for name in var_list]
+cs = [ROOT.TCanvas('c_'+name,'c_'+name) for name in var_list]
 for i in xrange(len(cs)):
     cs[i].cd()
     SignalVarsOutput[i].Draw('hist')
