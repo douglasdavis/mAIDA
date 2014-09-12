54cdf7454fc38a0f3213f3ecd828da47f50fb7c1
Douglas Davis <drd25@phy.duke.edu>
2014-09-12 19:16:15 -0400

    made setup script good for atl machines, gitignore edit

--- Setup.sh
+++ Setup.sh
@@ -1,5 +1,15 @@
 # mAIDA bash script to setup env
 
+if [ `hostname` == 'atl010.phy.duke.edu' ] || [ `hostname` == 'atl009.phy.duke.edu' ]; then
+    setupATLAS
+    source ${ATLAS_LOCAL_ROOT_BASE}/packageSetups/atlasLocalROOTSetup.sh --rootVersion 5.34.13-x86_64-slc6-gcc47-opt
+    export CXX=g++;
+    export CC=gcc;
+    export PATH=/home/drd25/localCMake/bin:$PATH
+    export PATH=/home/drd25/sw/atl_cmake/bin:$PATH
+    export BOOST_ROOT=/home/drd25/sw/atl_boost
+fi
+
 export TOPDIR=$PWD
 mkdir -p $TOPDIR/build
 cd $TOPDIR/build
