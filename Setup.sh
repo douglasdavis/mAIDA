36a3db8c5e6b8f3dcf673ffccc16d6d08b96d68b
Douglas Davis <drd25@phy.duke.edu>
2014-09-28 20:03:59 -0400

    new root version

--- Setup.sh
+++ Setup.sh
@@ -3,21 +3,16 @@
 # mAIDA bash script to setup env
 
 user=`whoami`
-
 hoststring=`hostname`
 if [[ $hoststring == *atl* ]]
 then
     setupATLAS
-    # using root version 5.34.13 on the SLC6 machines because
-    # i know it runs TMVA smoothly and doesnt have the
-    # PyROOT TLeaf bug that wasnt fixed until 5.34.20
-    # 
-    # set path to kind cmake28 installed in $HOME
-    # set boost_root for cmake to find it
-    root_version=5.34.13-x86_64-slc6-gcc47-opt
+    root_version=5.34.21-x86_64-slc6-gcc48-opt
     source ${ATLAS_LOCAL_ROOT_BASE}/packageSetups/atlasLocalROOTSetup.sh --rootVersion $root_version
     export CXX=g++;
     export CC=gcc;
+    # set path to kind cmake28 installed in $HOME
+    # set boost_root for cmake to find it
     export PATH=/home/$user/localCMake/bin:$PATH
     export PATH=/home/$user/sw/atl_cmake/bin:$PATH
     export BOOST_ROOT=/home/$user/sw/atl_boost
