7118111d49a4ecd4952a4c4e816849ae7ecb42b2
Douglas Davis <drd25@phy.duke.edu>
2014-10-30 15:58:21 -0400

    order of functions in utils trying to stay consistent

--- include/Utils.h
+++ include/Utils.h
@@ -67,6 +67,14 @@ namespace mAIDA {
   {
     return (eta1 - eta2);
   }
+
+  template <typename... T>
+  auto zip(const T&... containers) -> boost::iterator_range<boost::zip_iterator<decltype(boost::make_tuple(std::begin(containers)...))>>
+  {
+    auto zip_begin = boost::make_zip_iterator(boost::make_tuple(std::begin(containers)...));
+    auto zip_end   = boost::make_zip_iterator(boost::make_tuple(std::end(containers)...));
+    return boost::make_iterator_range(zip_begin, zip_end);
+  }
   
   enum pdg_codes {
 
@@ -76,15 +84,6 @@ namespace mAIDA {
     k_glu  = 21, k_gam  = 22, k_Z    = 23, k_W    = 24
 
   };
-
-  
-  template <typename... T>
-  auto zip(const T&... containers) -> boost::iterator_range<boost::zip_iterator<decltype(boost::make_tuple(std::begin(containers)...))>>
-  {
-    auto zip_begin = boost::make_zip_iterator(boost::make_tuple(std::begin(containers)...));
-    auto zip_end = boost::make_zip_iterator(boost::make_tuple(std::end(containers)...));
-    return boost::make_iterator_range(zip_begin, zip_end);
-  }
   
 }
 
