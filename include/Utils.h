f89e30a56221a76da45fed8590bef40791187aad
Doug Davis <drd25@phy.duke.edu>
2014-11-01 16:56:02 -0400

    inlining Utils function

--- include/Utils.h
+++ include/Utils.h
@@ -69,9 +69,9 @@ namespace mAIDA {
   {
     return (eta1 - eta2);
   }
-
+  
   template <typename... T>
-  auto zip(const T&... containers) -> boost::iterator_range<boost::zip_iterator<decltype(boost::make_tuple(std::begin(containers)...))>>
+  inline auto zip(const T&... containers) -> boost::iterator_range<boost::zip_iterator<decltype(boost::make_tuple(std::begin(containers)...))>>
   {
     auto zip_begin = boost::make_zip_iterator(boost::make_tuple(std::begin(containers)...));
     auto zip_end   = boost::make_zip_iterator(boost::make_tuple(std::end(containers)...));
