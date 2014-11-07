a61f8d49348d9ab315c8bbabc559da2dd852028c
Douglas Davis <drd25@phy.duke.edu>
2014-11-06 19:39:25 -0500

    new class -- LeptonPair

---
+++ include/LeptonPair.h
@@ -0,0 +1,60 @@
+#ifndef LeptonPair_h
+#define LeptonPair_h
+
+#include "TLorentzVector.h"
+#include "Lepton.h"
+
+namespace mAIDA {
+
+  class LeptonPair {
+
+  private:
+  
+    Lepton         _lepton_1;
+    Lepton         _lepton_2;
+    TLorentzVector _four_vector;
+    int            _pdg_sum;
+    float          _inv_mass;
+    bool           _isOS;
+    bool           _within_Z;
+    bool           _is_ee;
+    bool           _is_eu;
+    bool           _is_uu;
+    
+  public:
+
+    LeptonPair(const mAIDA::Lepton& l1, const mAIDA::Lepton& l2);
+    virtual ~LeptonPair();
+
+    void SetLeptons(const mAIDA::Lepton& l1, const mAIDA::Lepton& l2);
+
+    void Reload();
+    
+    const mAIDA::Lepton&  lepton1()     const;
+    const mAIDA::Lepton&  lepton2()     const;
+    const TLorentzVector& four_vector() const;
+
+    int   pdg_sum()  const;
+    float M()        const;
+    bool  isOS()     const;
+    bool  within_Z() const;
+    bool  is_ee()    const;
+    bool  is_uu()    const;
+    bool  is_eu()    const;    
+
+  };
+
+  inline const mAIDA::Lepton&  mAIDA::LeptonPair::lepton1()     const { return _lepton_1;    }
+  inline const mAIDA::Lepton&  mAIDA::LeptonPair::lepton2()     const { return _lepton_2;    }
+  inline const TLorentzVector& mAIDA::LeptonPair::four_vector() const { return _four_vector; }
+  inline       int             mAIDA::LeptonPair::pdg_sum()     const { return _pdg_sum;     }
+  inline       float           mAIDA::LeptonPair::M()           const { return _inv_mass;    }
+  inline       bool            mAIDA::LeptonPair::isOS()        const { return _isOS;        }
+  inline       bool            mAIDA::LeptonPair::within_Z()    const { return _within_Z;    } 
+  inline       bool            mAIDA::LeptonPair::is_ee()       const { return _is_ee;       }
+  inline       bool            mAIDA::LeptonPair::is_uu()       const { return _is_uu;       }
+  inline       bool            mAIDA::LeptonPair::is_eu()       const { return _is_eu;       }
+ 
+}
+
+#endif
---
+++ src/LeptonPair.cxx
@@ -0,0 +1,36 @@
+#include "LeptonPair.h"
+#include <cmath>
+
+mAIDA::LeptonPair::LeptonPair(const mAIDA::Lepton& l1, const mAIDA::Lepton& l2)
+{
+  _lepton_1    = l1;
+  _lepton_2    = l2;
+  Reload();
+}
+
+mAIDA::LeptonPair::~LeptonPair() {}
+
+void mAIDA::LeptonPair::SetLeptons(const mAIDA::Lepton& l1, const mAIDA::Lepton& l2)
+{
+  _lepton_1    = l1;
+  _lepton_2    = l2;
+  Reload();
+}
+
+void mAIDA::LeptonPair::Reload()
+{
+  _pdg_sum     = std::abs(_lepton_1.pdgId()) + std::abs(_lepton_2.pdgId());
+  _inv_mass    = (_lepton_1.four_vector()+_lepton_2.four_vector()).M();
+  _four_vector = _lepton_1.four_vector() + _lepton_2.four_vector();
+  _within_Z    = false;
+  _is_ee       = false;
+  _is_eu       = false;
+  _is_uu       = false;
+  
+  if ( _lepton_1.charge()*_lepton_2.charge() > 0 ) _isOS = false;
+  if ( _lepton_1.charge()*_lepton_2.charge() < 0 ) _isOS = true;
+  if ( fabs(_inv_mass - 91.1876e3) < 10e3 ) _within_Z = true;
+  if ( _pdg_sum == 22 ) _is_ee = true;
+  if ( _pdg_sum == 24 ) _is_eu = true;
+  if ( _pdg_sum == 26 ) _is_uu = true;
+}
